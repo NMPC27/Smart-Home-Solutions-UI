df8f06f0276c330793c682582043cadfa8ed91a2
nunocunha <nunocunha@ua.pt>
2024-01-07 15:43:30 +0000

    api Access-Control-Allow-Origin update

--- src/components/API.jsx
+++ src/components/API.jsx
@@ -16,58 +16,58 @@ export function doLogin(email,password) {
 
 export function signUp(data) {
 
-    return axios.post(endpoint+'/signUp', data);
+    return axios.post(endpoint+'/signUp', data, {headers: {"Access-Control-Allow-Origin": endpoint}});
 }
 
 export function getDevices() {
     let token = localStorage.getItem('token')
 
-  return axios.get(endpoint+'/devices', {headers: {"Authorization" : `Bearer ${token}`}});
+  return axios.get(endpoint+'/devices', {headers: {"Authorization" : `Bearer ${token}`, "Access-Control-Allow-Origin": endpoint}});
 }
 
 export function getRooms() {
     let token = localStorage.getItem('token')
 
-    return axios.get(endpoint+'/rooms', {headers: {"Authorization" : `Bearer ${token}`}});
+    return axios.get(endpoint+'/rooms', {headers: {"Authorization" : `Bearer ${token}`, "Access-Control-Allow-Origin": endpoint}});
 }
 
 export function getCards() {
     let token = localStorage.getItem('token')
 
-    return axios.get(endpoint+'/cards', {headers: {"Authorization" : `Bearer ${token}`}});
+    return axios.get(endpoint+'/cards', {headers: {"Authorization" : `Bearer ${token}`, "Access-Control-Allow-Origin": endpoint}});
 }
 
 export function postDevices(data) {
     let token = localStorage.getItem('token')
 
-    return axios.post(endpoint+'/devices', {data}, {headers: {"Authorization" : `Bearer ${token}`}});
+    return axios.post(endpoint+'/devices', {data}, {headers: {"Authorization" : `Bearer ${token}`, "Access-Control-Allow-Origin": endpoint}});
 }
 
 export function postRooms(data) {
     let token = localStorage.getItem('token')
 
-    return axios.post(endpoint+'/rooms', {data}, {headers: {"Authorization" : `Bearer ${token}`}});
+    return axios.post(endpoint+'/rooms', {data}, {headers: {"Authorization" : `Bearer ${token}`, "Access-Control-Allow-Origin": endpoint}});
 }
 
 export function postCards(data) {
     let token = localStorage.getItem('token')
 
-    return axios.post(endpoint+'/cards', {data}, {headers: {"Authorization" : `Bearer ${token}`}});
+    return axios.post(endpoint+'/cards', {data}, {headers: {"Authorization" : `Bearer ${token}`, "Access-Control-Allow-Origin": endpoint}});
 }  
 
 export function getEnergy(date) {
     let token = localStorage.getItem('token')
 
-    return axios.get(endpoint+`/energy?date=${date}`, {headers: {"Authorization" : `Bearer ${token}`}});
+    return axios.get(endpoint+`/energy?date=${date}`, {headers: {"Authorization" : `Bearer ${token}`, "Access-Control-Allow-Origin": endpoint}});
 }
 
 export function getFiles(file, download) {
     let token = localStorage.getItem('token')
 
     if (download){
-        return axios.get(endpoint+`/files?file=${file}`, {headers: {"Authorization" : `Bearer ${token}`}, responseType: 'blob'});
+        return axios.get(endpoint+`/files?file=${file}`, {headers: {"Authorization" : `Bearer ${token}`, "Access-Control-Allow-Origin": endpoint}, responseType: 'blob'});
     }else{
-        return axios.get(endpoint+`/files?file=${file}`, {headers: {"Authorization" : `Bearer ${token}`}});
+        return axios.get(endpoint+`/files?file=${file}`, {headers: {"Authorization" : `Bearer ${token}`, "Access-Control-Allow-Origin": endpoint}});
     }
 }
 
