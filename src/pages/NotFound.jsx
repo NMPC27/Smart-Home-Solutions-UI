540e7762b44f602d44243d05813ec73b1c1ab1b5
nunocunha <nunocunha@ua.pt>
2024-06-06 12:27:36 +0100

    not found page update

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -10,32 +10,37 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: "center",
   color: "#FFFFFF",
   borderRadius: "20px",
-  margin: "auto",
-  height: "82vh"
 }));
 
 export default function NotFound() {
   let navigate = useNavigate();
 
   return (
-    <Item>
-      <br />
-      <br />
-      <br />
-      <h1>404</h1>
-      <h2>Page Not Found</h2>
-      <Button
-        variant="contained"
-        sx={{ fontWeight: "bold" }}
-        onClick={() => navigate("/")}
-      >
-        Return Home
-      </Button>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-    </Item>
+    <div
+      style={{
+        width: "90vw",
+        height: "90vh",
+        backgroundImage: "url(https://source.unsplash.com/random?wallpapers)",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        borderRadius: "20px",
+        display:"flex", 
+        justifyContent:"center", 
+        alignItems:"center"
+      }}
+    >
+      <Item>
+        <h1>404</h1>
+        <h2>Page Not Found</h2>
+        <Button
+          variant="contained"
+          sx={{ fontWeight: "bold" }}
+          onClick={() => navigate("/")}
+        >
+          Return Home
+        </Button>
+      </Item>
+    </div>
   );
 }
